15f5d97c7d9186b4ff41c8121f846e707626d73f
Hvanderklis <henk@hvanderklis.nl>
2017-04-03 15:26:33 +0200

    added files sightseeing

---
+++ src/AlbaBundle/Controller/WebsiteController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Henk van der Klis
+ * Date: 4/3/2017
+ * Time: 15:19
+ */
+
+namespace AlbaBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class WebsiteController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function sightseeingAction()
+    {
+        return $this->render("@Alba/sightseeing.html.twig");
+    }
+}

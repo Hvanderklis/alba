537af1cf53ffa36324f5697cb5513652ea8a7e89
Hvanderklis <henk@hvanderklis.nl>
2017-04-03 16:09:31 +0200

    made sightseeing page.

--- src/AlbaBundle/Controller/WebsiteController.php
+++ src/AlbaBundle/Controller/WebsiteController.php
@@ -15,7 +15,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class WebsiteController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/sightseeing")
      */
     public function sightseeingAction()
     {

8c937e990983e817742abb4b3b25e0508002109e
Richard Binda <rb_97@live.nl>
2017-04-05 14:42:35 +0200

    Test voor branch

---
+++ src/AlbaBundle/Controller/WebsiteController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Richh
+ * Date: 5-4-2017
+ * Time: 11:05
+ */
+
+namespace AlbaBundle\Controller;
+
+use AlbaBundle\Entity\Kamer;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WebsiteController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/room")
+     */
+    public function kameraction(){
+        return $this->render('AlbaBundle:website:kamer.html.twig');
+    }
+
+    public function showkameraction(){
+        return $this->render('');
+    }
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/rooms", name="RoomsIndex")
+     */
+    public function IndexAction(){
+        $em = $this->getDoctrine()->getManager();
+        $repositoryRooms = $em->getRepository("AlbaBundle:Kamer");
+        $Rooms = $repositoryRooms->findAll();
+
+        return $this->render("AlbaBundle:website:kamer.html.twig", [
+            "Kamer" => $Rooms
+        ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/{id}", name="RoomsShow")
+     */
+    public function ShowAction(Kamer $kamer){
+        return $this->render("@Alba/website/show.html.twig", [
+            'Kamer' => $kamer
+        ]);
+    }
+
+}

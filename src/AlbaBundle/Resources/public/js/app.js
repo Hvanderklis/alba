2c6fba955d511ca2ae68ee8e1a05bf4dc0227c20
Hvanderklis <henk@hvanderklis.nl>
2017-04-05 14:01:25 +0200

    added google maps, changed some things in text. Only some icons left.

--- src/AlbaBundle/Resources/public/js/app.js
+++ src/AlbaBundle/Resources/public/js/app.js
@@ -1,1 +1,72 @@
-$(document).foundation()
+function initialize()
+{
+
+    var locations = [
+        ['Knockando Woolmill', 57.465735, -3.355088, 3],
+        ['Linn Falls', 57.461174, -3.226149, 4],
+        ['Aberlour Distillery', 57.467516, -3.228945, 2],
+        ['The Macallan Distillery', 57.484647, -3.206955, 1],
+        ['Aberlour', 57.474911, -3.216983, 5]
+    ];
+
+    var latlng = new google.maps.LatLng(57.474911,-3.216983);
+    var latlng2 = new google.maps.LatLng(57.474911,-3.216983);
+    var myOptions =
+        {
+            zoom: 12,
+            center: latlng,
+            scrollwheel: false,
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            draggable: false,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+
+    var myOptions2 =
+        {
+            zoom: 14,
+            center: latlng2,
+            scrollwheel: false,
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            draggable: false,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+
+    var map = new google.maps.Map(document.getElementById("sight"), myOptions);
+
+    var map2 = new google.maps.Map(document.getElementById("map"), myOptions2);
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+
+    var myMarker2 = new google.maps.Marker(
+        {
+            position: latlng2,
+            map: map2
+        });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map, marker);
+        }
+    })(marker, i));
+}

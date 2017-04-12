3cdf9302f22ae8b6ef64679574a6a4c0fb4a2a59
Hvanderklis <henk@hvanderklis.nl>
2017-04-12 12:44:58 +0200

    Added a working form

--- src/AlbaBundle/Controller/WebsiteController.php
+++ src/AlbaBundle/Controller/WebsiteController.php
@@ -11,17 +11,54 @@ namespace AlbaBundle\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class WebsiteController extends Controller
 {
     /**
      * @Route("/contact")
      */
-    public function contacAction()
+    public function formExampleAction(Request $request)
     {
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('email', TextType::class)
+            ->add('message', TextType::class)
+            ->add('submit', SubmitType::class, [
+                'label' => 'Submit!',
+                'attr'  => [
+                    'class' => 'button primary'
+                ]
+            ])
+            ->getForm();
 
-        return $this->render("@Alba/contact.html.twig");
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $name = $form->getData()['name'];
+            $email = $form->getData()['email'];
+            $message = $form->getData()['message'];
+
+            $this->sendMail($name, $email, $message);
+        }
+
+        return $this->render('@Alba/contact.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    private function sendMail($body)
+    {
+        $mail = \Swift_Message::newInstance()
+            ->setSubject('test mail')
+            ->setFrom('test1@test1.com')
+            ->setTo('test2@test2.com')
+            ->setBody($body)
+        ;
+
+        $this->get('mailer')->send($mail);
     }
 }

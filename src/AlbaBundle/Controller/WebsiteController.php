9d703bb59ff886c223a3f2c0ac2d3930d707d72a
Lucinaaa <leeuwin.1994@hotmail.com>
2017-04-12 22:07:17 +0200

    removed head comment of zo iets

--- src/AlbaBundle/Controller/WebsiteController.php
+++ src/AlbaBundle/Controller/WebsiteController.php
@@ -14,7 +14,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class WebsiteController extends Controller
 {
     /**
-<<<<<<< HEAD
      * @Route("/sightseeing")
      */
     public function sightseeingAction()

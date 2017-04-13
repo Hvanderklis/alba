1a9dff9e65f7892270c7989258dfa7229b77a8f3
Mohamed abdi <1019996@idcollege.nl>
2017-04-13 21:43:48 +0200

    Mail works

--- src/AlbaBundle/Controller/WebsiteController.php
+++ src/AlbaBundle/Controller/WebsiteController.php
@@ -4,7 +4,7 @@ namespace AlbaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 class WebsiteController extends Controller
 {
     /**
@@ -14,20 +14,31 @@ class WebsiteController extends Controller
     {
         return $this->render("@Alba/sightseeing.html.twig");
     }
-     /*
-     * @Route("/contact")
-     */
-    public function contacAction()
-    {
-        return $this->render("@Alba/contact.html.twig");
-    }
-
      /**
-     * @Route("/sightseeing")
+     * @Route("/sendMail", name="mail")
      */
-
-    public function sightseeingAction()
+    public function contacAction(Request $request)
     {
-        return $this->render("@Alba/sightseeing.html.twig");
+        if($request->getMethod() == "POST") {
+
+            $Subject = $request->get("Subject");
+            $email = $request->get("Email");
+            $message = $request->get("message");
+
+            $mailer = $this->container->get('mailer');
+            $transport = \Swift_SmtpTransport::newInstance('smtp.mailtrap.io', 465, 'ssl')
+                ->setUsername('6b85cd05068089')
+                ->setPassword('10aaf099663b37');
+
+            $mailer = \Swift_Mailer::newInstance($transport);
+
+            $message = \Swift_Message::newInstance('Test')
+                ->setSubject($Subject)
+                ->setFrom('token.jetset@gmail.com')
+                ->setTo($email)
+                ->setBody($message);
+            $this->get('mailer')->send($message);
+        }
+        return $this->render("@Alba/mail.html.twig");
     }
 }
